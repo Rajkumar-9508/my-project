570ccc60c592c1c613e64380fd58b4f8510257ed
rajkumar <berarajkumar713@gmail.com>
2025-08-29 20:53:19 +0530

    Add professional README for SkillSync

--- my-backend/src/server.js
+++ my-backend/src/server.js
@@ -5,6 +5,10 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("Hello Backend is running 🚀");
+});
+
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port http://localhost:${PORT}`);
 });
