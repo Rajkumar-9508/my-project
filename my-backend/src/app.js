03b67e5b2b6662101a31d8e6654a6aefc9c3974b
rajkumar <berarajkumar713@gmail.com>
2025-08-31 10:03:41 +0530

    Add professional README for SkillSync

--- my-backend/src/app.js
+++ my-backend/src/app.js
@@ -1,15 +1,35 @@
-import express from "express";
-import cors from "cors";
-const app = express();
-app.use(
-    cors({
-        origin: process.env.CORS_ORIGIN,
-        credentials:true
-    })
-)
-//common middleware
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended:true, limit:"16kb"}))
-app.use(express.static("public"))
+// import express from "express";
+// import cors from "cors";
+// const app = express();
+// app.use(
+//     cors({
+//         origin: process.env.CORS_ORIGIN,
+//         credentials:true
+//     })
+// )
+// //common middleware
+// app.use(express.json({limit: "16kb"}))
+// app.use(express.urlencoded({extended:true, limit:"16kb"}))
+// app.use(express.static("public"))
 
-export default app;
+// export default app;
+
+
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./db/index");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// DB Connect
+connectDB();
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("API is running & DB connected 🚀");
+});
+
+module.exports = app;
--- my-backend/src/server.js
+++ my-backend/src/server.js
@@ -1,14 +1,24 @@
-import dotenv from "dotenv";
-import app from "./app.js";
+// import dotenv from "dotenv";
+// import app from "./app.js";
 
-dotenv.config();
+// dotenv.config();
+
+// const PORT = process.env.PORT || 5000;
+
+// app.get("/", (req, res) => {
+//   res.send("Hello Backend is running 🚀");
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on port http://localhost:${PORT}`);
+// });
+
+
+
+const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello Backend is running 🚀");
-});
-
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port http://localhost:${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
